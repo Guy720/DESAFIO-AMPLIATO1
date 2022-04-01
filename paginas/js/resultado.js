e47927dfa5f70c96cb9a82db9da3b421b3690708
Guy720 <101023869+Guy720@users.noreply.github.com>
2022-04-01 00:20:06 -0300

    Script para colocar texto na tag html
    Após a dica do Rodrigo, consegui concluir o script para quando o usuário fazer o cadastro, na página resultado.html, irá aparecer todos os campos preenchidos pelo usuário

---
+++ paginas/js/resultado.js
@@ -0,0 +1,36 @@
+
+    const nome = document.getElementById('nome').innerHTML='Nome: Kayke';
+    urlParams.get('nome');
+
+    const email = document.getElementById('email').innerHTML='Email: kayketorres72@gmail.com';
+    urlParams.get('email');
+
+    const senha = document.getElementById('senha').innerHTML='Senha: 1223456789';
+    urlParams.get('senha');
+
+    const idade = document.getElementById('idade').innerHTML='Idade: 20';
+    urlParams.get('idade');
+
+    const masculino = document.getElementById('masculino').innerHTML='Gênero: Masculino';
+    urlParams.get('masculino');
+
+    const feminino = document.getElementById('feminino').innerHTML='Gênero: Feminino';
+    urlParams.get('feminino');
+
+    const outro = document.getElementById('outro').innerHTML='Gênero: Outro';
+    urlParams.get('outro');
+
+    const futebol = document.getElementById('futebol').innerHTML='Hobbie: Futebol';
+    urlParams.get('futebol');
+
+    const pipa = document.getElementById('pipa').innerHTML='Hobbie: Pipa';
+    urlParams.get('pipa');
+
+    const videogame = document.getElementById('videogame').innerHTML='Hobbie: Videogame';
+    urlParams.get('videogame');
+
+    const tipo = document.getElementById('tipo2').innerHTML='Tipo sanguíneo: O';
+    urlParams.get('tipo2');
+
+    const mensagem = document.getElementById('mensagem').innerHTML='Mensagem: Olá';
+    urlParams.get('mensagem');
